f05efa6f7d0ba0f5f46373eebbf00d84e7b8900a
YETC7 <tyler_726@live.com>
2017-06-12 18:12:42 -0700

    retrieve information using ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,34 @@
+// Declaring variables
+var eventList = [];
+var eventLimit = 7;
+
+// Query string
 var queryURL = "https://api.seatgeek.com/2/events?";
 var clientID = "client_id=NzgwMTQ1M3wxNDk3MTIxNzA4LjE4";
 var subject = "&q=sports"
 
 // https://api.seatgeek.com/2/events/739515?callback=fireEvent&client_id=NzgwMTQ1M3wxNDk3MTIxNzA4LjE4
 
+// Ajax call
 $.ajax({
 	url: queryURL + clientID + subject,
 	method: 'GET'
 }).done(function(response){
 	console.log(response);
+	var resEvents = response.events;
+	for (var i = 0; i < resEvents.length; i++) {
+		eventList.push({
+			title:resEvents[i].title,
+			type:resEvents[i].type,
+			address:resEvents[i].venue.address,
+			location:resEvents[i].venue.extended_address,
+			date:resEvents[i].datetime_local
+		});
+
+		if(eventList.length === eventLimit) {
+			i = resEvents.length;
+		}
+	}
+	
+	console.log(eventList);
 });
