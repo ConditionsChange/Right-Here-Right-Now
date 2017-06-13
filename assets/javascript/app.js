99087ea82050ab6ec426c5ec6bb2d77e4e84d5e0
YETC7 <tyler_726@live.com>
2017-06-12 22:11:13 -0700

    find events by location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,27 +2,38 @@
 var eventList = [];
 var eventLimit = 7;
 
+var currentTime = moment().utc().format("YY-MM-DD");
+console.log(currentTime);
+
+var city = "Los+Angeles";
+var state = "CA";
+
 // Query string
 var queryURL = "https://api.seatgeek.com/2/events?";
 var clientID = "client_id=NzgwMTQ1M3wxNDk3MTIxNzA4LjE4";
-var subject = "&q=sports"
+// var subject = "&q=sports"
+var datetime = "&datetime_utc.gt=" + currentTime;
+var loc = "&venue.state=" + state + "&venue.city=" + city;
+// var venueState & venueCity
 
 // https://api.seatgeek.com/2/events/739515?callback=fireEvent&client_id=NzgwMTQ1M3wxNDk3MTIxNzA4LjE4
 
 // Ajax call
 $.ajax({
-	url: queryURL + clientID + subject,
+	url: queryURL + clientID + datetime + loc,
 	method: 'GET'
 }).done(function(response){
 	console.log(response);
 	var resEvents = response.events;
+
 	for (var i = 0; i < resEvents.length; i++) {
+
 		eventList.push({
-			title:resEvents[i].title,
-			type:resEvents[i].type,
-			address:resEvents[i].venue.address,
-			location:resEvents[i].venue.extended_address,
-			date:resEvents[i].datetime_local
+			title: resEvents[i].title,
+			type: resEvents[i].type,
+			address: resEvents[i].venue.address,
+			location: resEvents[i].venue.extended_address,
+			date: resEvents[i].datetime_local
 		});
 
 		if(eventList.length === eventLimit) {
